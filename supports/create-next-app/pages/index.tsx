7d065217d892e5332da8039dffbe3fa88d313cab
Bunlong <bunlong.van@gmail.com>
2022-06-05 21:24:26 +0700

    Update example

--- supports/create-next-app/pages/index.tsx
+++ supports/create-next-app/pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useBarcode } from 'next-barcode';
+
+import { useBarcode } from 'next-barcode'
 
 const Home: NextPage = () => {
   const { inputRef } = useBarcode({
     value: 'next-barcode',
-    options: {
-      background: '#ccffff',
-    }
-  })
+  });
 
-  return <svg ref={inputRef} />
+  return (
+    <>
+      <img ref={inputRef} />
+    </>
+  )
 }
 
 export default Home
